f9355280beee19dbeb3f2634fb322dfb7009fef3
Ritikesh <ritikesh.g@freshdesk.com>
2017-12-20 17:26:08 +0530

    added support to cache nils

--- lib/memoize_until/store.rb
+++ lib/memoize_until/store.rb
@@ -2,29 +2,49 @@ class MemoizeUntil
 
 	class Store
 
-		def self.set(key, moment, value)
-			@key_val[key][moment] = value
+		class << self
+
+			def set(key, moment, value)
+				@key_val[key][moment] = set_nil(value)
+			end
+
+			def get(key, moment)
+				@key_val[key][moment]
+			end
+
+			def clear_all(key)
+				@key_val[key] = {}
+			end
+
+			def clear_for(key, moment)
+				@key_val[key][moment] = nil
+			end
+
+			def fetch(key, kind, &block)
+				now = Time.now.send(kind)
+				value = get(key, now)
+				return unset_nil(value) if value
+				clear_all(key)
+				set(key, now, yield)
+			end
+
+			private
+				def set_nil(value)
+					value.nil? ? NullObject.instance : value
+				end
+
+				def unset_nil(value)
+					value.is_a?(NullObject) ? nil : value
+				end
+		end
+	end
+
+	class NullObject
+		
+		def self.instance
+			@@null_object ||= NullObject.new
 		end
 
-		def self.get(key, moment)
-			@key_val[key][moment]
-		end
-
-		def self.clear_all(key)
-			@key_val[key] = {}
-		end
-
-		def self.clear_for(key, moment)
-			@key_val[key][moment] = nil
-		end
-
-		def self.fetch(key, kind, &block)
-			now = Time.now.send(kind)
-			value = get(key, now)
-			return value if value
-			clear_all(key)
-			set(key, now, yield)
-		end
 	end
 
 end
