6df5dcbef34839ded4b54807bba691d44762d141
Ritikesh <ritikesh.g@freshdesk.com>
2018-05-11 01:58:43 +0530

    reindentation fix

--- lib/memoize_until/extensions.rb
+++ lib/memoize_until/extensions.rb
@@ -1,12 +1,12 @@
 class MemoizeUntil
-
+	
 	class NotImplementedError < StandardError; end
-
+	
 	class NullObject
 		
 		def self.instance
 			@@null_object ||= NullObject.new
 		end
-
-    end
+		
+	end
 end
