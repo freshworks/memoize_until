1ef5bcb22af41c984420abfd0c8a47faf1cfee37
Ruban Thilak <ruban.thilak@freshworks.com>
2023-04-21 00:34:42 +0530

    Update base.rb

--- lib/memoize_until/base.rb
+++ lib/memoize_until/base.rb
@@ -3,8 +3,8 @@
 class MemoizeUntil
   memoizable_attributes = YAML.load_file("#{__dir__}/config/defaults.yml")
 
-  if defined?(Rails) && File.exist?(Rails.root.join('/config/memoize_until.yml'))
-    memoizable_attributes.deep_merge!(YAML.load_file(Rails.root.join('/config/memoize_until.yml')))
+  if defined?(Rails) && File.exist?(Rails.root.join('config/memoize_until.yml'))
+    memoizable_attributes.deep_merge!(YAML.load_file(Rails.root.join('config/memoize_until.yml')))
   end
 
   TYPE_FACTORY = {}
